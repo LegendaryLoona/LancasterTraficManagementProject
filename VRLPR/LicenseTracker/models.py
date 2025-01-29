fc5f036acb937654f695e5c44d15d4d8215bfd17
maomao7788 <maomaorc@foxmail.com>
2025-01-29 20:09:09 +0100

    Update models.py

--- VRLPR/LicenseTracker/models.py
+++ VRLPR/LicenseTracker/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.db.models import JSONField
 class Person(models.Model):
     name = models.CharField(max_length=50, default=None)
-    birth_date = models.DateField()
-    age = models.IntegerField(blank=True, null=True, default=None)
-    address = models.CharField(max_length=50, default=None)
+    birth_date = models.DateField(null=True, blank=True)
+    # age = models.IntegerField(blank=True, null=True, default=None)
+    address = models.CharField(max_length=50, null=True, blank=True)
+
     def owned_cars(self):
         return [car.number for car in self.cars.all()]
 
@@ -12,9 +12,7 @@ class License(models.Model):
     number = models.CharField(max_length=50, unique=True)
     issue_date = models.DateField()
     expiry_date = models.DateField()
-    person = models.OneToOneField(Person, on_delete=models.CASCADE, primary_key=True,)
-
-
+    person = models.OneToOneField(Person, on_delete=models.CASCADE, primary_key=True)
 
 class Junktion(models.Model):
     address = models.CharField(max_length=50, unique=True)
@@ -30,9 +28,9 @@ class Junktion(models.Model):
             return "High traffic"
 
 class Car(models.Model):
-    number = models.CharField(max_length=50, default=None)
-    manufacturer = models.CharField(max_length=50, default=None)
-    model = models.CharField(max_length=50, default=None)
-    color = models.CharField(max_length=50, default=None)
+    number = models.CharField(max_length=50, null=True, default=None)
+    manufacturer = models.CharField(max_length=50, null=True, default=None)
+    model = models.CharField(max_length=50, null=True, default=None)
+    color = models.CharField(max_length=50, null=True, default=None)
     owner = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True, related_name='cars')
     junction = models.ForeignKey(Junktion, on_delete=models.SET_NULL, null=True, related_name='cars')
