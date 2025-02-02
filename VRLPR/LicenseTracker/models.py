0a48142514c3bfeea43ac1c22db68880fb15a290
maomao7788 <maomaorc@foxmail.com>
2025-02-02 20:22:18 +0100

    Update models.py

--- VRLPR/LicenseTracker/models.py
+++ VRLPR/LicenseTracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Person(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
     name = models.CharField(max_length=50, default=None)
