b75e205ce2ab627bc3ff12f39ea43571dbd89362
maomao7788 <maomaorc@foxmail.com>
2025-02-05 15:11:37 +0100

    Update settings.py

--- VRLPR/CarProject/settings.py
+++ VRLPR/CarProject/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for CarProject project.
 
@@ -126,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sender email address'  
+EMAIL_HOST_PASSWORD = 'wwrfondyfipydpzn' 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
